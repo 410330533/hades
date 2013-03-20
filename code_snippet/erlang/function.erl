a24c2bea77d9dd3f8639cea5da4494bc4ac868ad
mahone3297 <329730566@qq.com>
2013-03-21 07:30:27 +0800

    update erlang

---
+++ code_snippet/erlang/function.erl
@@ -0,0 +1,18 @@
+-module(function).
+-export([mirror/1]).
+-export([number/1]).
+-export([another_factorial/1]).
+-export([another_fib/1]).
+
+mirror(Anything) -> Anything.
+
+number(one) -> 1;
+number(two) -> 2;
+number(three) -> 3.
+
+another_factorial(0) -> 1;
+another_factorial(N) -> N * another_factorial(N - 1).
+
+another_fib(0) -> 1;
+another_fib(1) -> 1;
+another_fib(N) -> another_fib(N-1) + another_fib(N-2).
--- code_snippet/erlang/test.erl
+++ code_snippet/erlang/test.erl
@@ -3,5 +3,26 @@ List = [1, 2, 3].
 Tuple = {comic_strip, {name, "Calvin and Hobbes"}, {character, "Spaceman Spiff"}}.
 Person = {person, {name, "Agent Smith"}, {profession, "Killing programs"}}.
 {person, {name, Name}, {profession, Profession}} = Person.
-[Head | Tail] = [1, 2, 3]
-[One, Two | Rest] = [1, 2, 3]
+[Head | Tail] = [1, 2, 3].
+[One, Two | Rest] = [1, 2, 3].
+
+% bit match
+W = 1.
+X = 2.
+Y = 3.
+Z = 4.
+All = <<W:3, X:3, Y:5, Z:5>>.
+<<A:3, B:3, C:5, D:5>> = All.
+
+Animal = "dog".
+case Animal of
+    "dog" -> underdog;
+    "cat" -> thundercat;
+    _ -> something_else
+end.
+
+if
+    x > 0 -> positive;
+    x < 0 -> negative;
+    true -> zero
+end.
