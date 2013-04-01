2753a0ebe5c51d13245dbaed07e9c2a940848e63
mahone3297 <329730566@qq.com>
2013-04-02 07:03:16 +0800

    update code_snippet/clojure/test.clj

--- code_snippet/clojure/test.clj
+++ code_snippet/clojure/test.clj
@@ -95,3 +95,37 @@
 
 (filter odd? v)
 (filter #(< % 3) v)
+
+; loop & recur
+(defn size [v]
+    (if (empty? v)
+        0
+        (inc (size (rest v)))))
+(size [1 2 3])
+; (loop [x x-initial-value, y y-initial-value] (do-something-with x y))
+
+; list
+(every? number? [1 2 3 :four])
+(some nil? [1 2 nil])
+(not-every? odd? [1 3 5])
+(not-any? number? [:one :two :three])
+
+(def words ["luke" "chewie" "han" "lando"])
+(filter (fn [word] (> (count word) 4)) words)
+
+(map (fn [x] (* x x)) [1 1 2 3 5])
+
+(def colors ["red" "blue"])
+(def toys ["block" "car"])
+(for [x colors] (str "I like " x))
+(for [x colors, y toys] (str "I like " x " " y "s"))
+
+(defn small-word? [w] (< (count w) 4))
+(for [x colors, y toys, :when(small-word? y)]
+    (str "I like " x " " y "s"))
+
+(reduce + [1 2 3 4])
+(reduce * [1 2 3 4 5])
+(sort [3 1 2 4])
+(defn abs [x] (if (< x 0) (- x) x))
+(sort-by abs [-1 -4 3 2])
