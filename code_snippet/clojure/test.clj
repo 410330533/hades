094cf444b9e7319b64ad2e59e70870ba77d755af
mahone3297 <329730566@qq.com>
2013-04-02 22:39:37 +0800

    update code_snippet/clojure/test.clj

--- code_snippet/clojure/test.clj
+++ code_snippet/clojure/test.clj
@@ -129,3 +129,24 @@
 (sort [3 1 2 4])
 (defn abs [x] (if (< x 0) (- x) x))
 (sort-by abs [-1 -4 3 2])
+
+; delay calculate
+(range 1 10)
+(range 1 10 3)
+(range 10)
+
+(take 3 (repeat "Use the Force, Luke"))
+(take 5 (cycle [:lather :rinse :repeat]))
+(take 5 (drop 2 (cycle [:lather :rinse :repeat])))
+(->> [:lather :rinse :repeat] (cycle) (drop 2) (take 5))
+(take 5 (interpose :and (cycle [:lather :rinse :repeat])))
+(take 20 (interleave (cycle (range 2)) (cycle (range 3))))
+(take 5 (iterate inc 1))
+(take 5 (iterate dec 0))
+
+(defn fib-pair [[a b]] [b (+ a b)])
+(fib-pair [3 5])
+(take 5 (map first (iterate fib-pair [1 1])))
+(nth (map first (iterate fib-pair [1 1])) 50)
+(defn factorial [n] (apply * (take n (iterate inc 1))))
+(factorial 5)
