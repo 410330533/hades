a7265dc3cba4f57bcab74391b559ee2b524c5304
mahone3297 <329730566@qq.com>
2013-04-09 23:38:17 +0800

    update code_snippet/haskell/test.hs

--- code_snippet/haskell/test.hs
+++ code_snippet/haskell/test.hs
@@ -1,3 +1,28 @@
+module Main where
+    double :: Integer -> Integer
+    double x = x + x
+-- :load test.hs
+-- double 5
+
+let fact x = if x == 0 then 1 else fact (x - 1) * x
+
+-- factorial.hs
+    factorial :: Integer -> Integer
+    factorial 0 = 1
+    factorial x = x * factorial (x - 1)
+
+-- factorial_with_guard.hs
+    factorial_with_guard :: Integer -> Integer
+    factorial_with_guard x
+        | x > 1 = x * factorial_with_guard (x - 1)
+        | otherwise = 1
+
+-- fib.hs
+    fib :: Integer -> Integer
+    fib 0 = 1
+    fib 1 = 1
+    fib x = fib(x - 1) + fib(x - 2)
+
 4 + 4
 4 + 1.0
 "hello" ++ " world"
