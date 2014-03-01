4ec3610d6212de18756778a70d0389280369c7ff
mahone3297 <329730566@qq.com>
2014-03-01 23:43:53 +0800

    update code_snippet/ruby/mongo.rb

--- code_snippet/ruby/mongo.rb
+++ code_snippet/ruby/mongo.rb
@@ -1,24 +1,19 @@
+# https://github.com/mongodb/mongo-ruby-driver
+
 require 'mongo'
 
 include Mongo
 
-mongo_client = MongoClient.new('localhost', 27017)
-#  puts mongo_client.database_names
-db = mongo_client.db('test')
-coll = db.collection('testData')
-doc = {"name" => "MongoDB", "type" => "database", "count" => 1, "info" => {"x" => 203, "y" => '102'}}
-# id = coll.insert(doc)
-# puts id
-# puts coll.find_one
+@client = MongoClient.new('localhost', 27017)
+@db     = @client['test']
+@coll   = @db['testData']
 
-coll.find.each do |row|
-  puts row.inspect
+@coll.remove
+
+3.times do |i|
+  @id = @coll.insert({'a' => i+1})
+  puts "id = #{@id}"
 end
 
-exit
-doc["name"] = "MongoDB Ruby"
-coll.update({"_id" => id}, doc)
-
-coll.update({"_id" => "5295b24102c0ff86ce394488"}, {"xx" => "oo"})
-puts 'updated...'
-puts coll.find("_id" => "5295b24102c0ff86ce394488").to_a
+puts "There are #{@coll.count} records. Here they are:"
+@coll.find.each { |doc| puts doc.inspect }
