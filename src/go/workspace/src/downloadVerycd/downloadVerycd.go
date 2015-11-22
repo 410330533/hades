e31823cd9f3600c72aa2ff6e8d8752db7f978560
mahone3297 <329730566@qq.com>
2015-11-22 18:53:43 +0800

    update downloadVerycd.go

--- src/go/workspace/src/downloadVerycd/downloadVerycd.go
+++ src/go/workspace/src/downloadVerycd/downloadVerycd.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
+func req(c chan int) {
+	resp, err := http.Get("http://www.verycd.com/sto/music/china/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(body))
+	c <- 1
+}
+
 func main() {
-	fmt.Printf("hello go .... download verycd\n")
+	log.Println("download verycd start....")
+
+	c := make(chan int)
+	go req(c)
+	<-c
+
+	log.Println("download verycd end....")
 }
