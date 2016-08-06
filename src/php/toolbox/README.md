4a09d240dca02b99b829174b3f1f4673845a9ddd
mahone3297 <329730566@qq.com>
2016-08-07 06:02:12 +0800

    rm php useless code.

--- src/php/toolbox/README.md
+++
@@ -1,4 +0,0 @@
-toolbox
-=======
-
-A Symfony project created on November 23, 2015, 10:17 pm.
--- src/php/toolbox/src/Yy/ToolboxBundle/YyToolboxBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Yy\ToolboxBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class YyToolboxBundle extends Bundle
-{
-}
