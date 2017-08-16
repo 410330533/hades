df7cf8f4ae7379183ccccc2d70f9978a8230678e
mahone3297 <329730566@qq.com>
2017-08-16 23:36:02 +0800

    rename files with multiple pattern

--- src/php/rename_meizu_photos.php
+++ src/php/rename_meizu_photos.php
@@ -3,26 +3,33 @@
 $dir = $argv[1];
 chdir($dir);
 
-// build array according to the files.
-$map = [];
-foreach (glob('*.jpg') as $filename) {
-    $day = '201' . substr($filename, 1, 5);
-    if ( ! isset($map[$day]))
-        $map[$day] = [];
+$patterns = ['*.jpg', '*.gif', '*.mp4'];
 
-    $map[$day][] = [
-        'old' => $filename,
-        'new' => $day . '/' . $filename,
-    ];
-}
+rename_meizu_photos($patterns);
 
-// rename
-foreach ($map as $dir => $items) {
-    if ( ! file_exists($dir))
-        mkdir($dir);
+function rename_meizu_photos(array $patterns) {
+    foreach ($patterns as $pattern) {
+        $map = [];
+        foreach (glob($pattern) as $filename) {
+            $day = '201' . substr($filename, 1, 5);
+            if ( ! isset($map[$day]))
+                $map[$day] = [];
 
-    foreach ($items as $item) {
-        rename($item['old'], $item['new']);
-        echo sprintf("rename(%s, %s)\n", $item['old'], $item['new']);
+            $map[$day][] = [
+                'old' => $filename,
+                'new' => $day . '/' . $filename,
+            ];
+        }
+
+        // rename
+        foreach ($map as $dir => $items) {
+            if ( ! file_exists($dir))
+                mkdir($dir);
+
+            foreach ($items as $item) {
+                rename($item['old'], $item['new']);
+                echo sprintf("rename(%s, %s)\n", $item['old'], $item['new']);
+            }
+        }
     }
 }
