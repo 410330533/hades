3cfbd1848cef38ce4f9a5b915a7f7fc77aaa4f1f
mahone3297 <329730566@qq.com>
2018-06-18 11:21:47 +0800

    update EgCmd.py

--- src/python/EgCmd.py
+++ src/python/EgCmd.py
@@ -1,22 +1,42 @@
+import os
 from cmd import Cmd
-# import cmd, sys
 
 class MyPrompt(Cmd):
-    def do_hello(self, args):
+    # prompt = '>'
+    # intro = 'intro is here...'
+
+    # doc_header = 'doc_header'
+    # misc_header = 'misc_header'
+    # undoc_header = 'undoc_header'
+
+    # ruler = '-'
+
+    def do_hello(self, line):
         """Says hello. If you provide a name, it will greet you with it."""
-        if len(args) == 0:
+        if len(line) == 0:
             name = 'stranger'
         else:
-            name = args
+            name = line
         print('hello %s' % (name))
 
-    def do_quit(self, args):
+    def do_go(self, version, servers = 'app3 app4'):
+        """do go..."""
+        version = int(version)
+        print('go go go... version = %d, servers = %s' % (version, servers))
+
+    def do_quit(self, line):
         """Quits the program."""
         print('quit...')
         raise SystemExit
 
+    # def do_EOF(self, line):
+        # return True
+
+    def do_shell(self, line):
+        """Run a shell command"""
+        print("running shell command:", line)
+        output = os.popen(line).read()
+        print(output)
+
 if __name__ == '__main__':
-    p = MyPrompt()
-    # p.prompt = '>'
-    # p.cmdloop('Starting prompt...')
-    p.cmdloop()
+    MyPrompt().cmdloop()
