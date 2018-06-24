6f8e03d2df61e616e3e3225a8f47eff842ed4ecf
mahone3297 <329730566@qq.com>
2018-06-24 17:17:40 +0800

    update src/python/rename.py

--- src/python/rename.py
+++ src/python/rename.py
@@ -7,17 +7,17 @@ from os.path import join, basename, exists
 from os import mkdir, chdir, rename
 from shutil import rmtree, move, copy, copytree
 
-def rename_files(ext):
-    i =  0
-    for f in glob.glob('*.' + ext):
-        name = basename(f)
-        d = '201'+name[1:6]
-        if not exists(d):
-            mkdir(d)
-        rename(name, join(d, name))
+# def rename_files(ext):
+#     i =  0
+#     for f in glob.glob('*.' + ext):
+#         name = basename(f)
+#         d = '201'+name[1:6]
+#         if not exists(d):
+#             mkdir(d)
+#         rename(name, join(d, name))
 
-        i += 1
-        print("%d rename from %s, to %s" % (i, name, join(d, name)))
+#         i += 1
+#         print("%d rename from %s, to %s" % (i, name, join(d, name)))
 
 def replace_filename():
     for f in glob.glob('**/*.*', recursive=True):
@@ -26,7 +26,7 @@ def replace_filename():
         print('old = %s, new = %s' % (old, new))
         rename(old, new)
 
-def move_ml_photo(dir):
+def mv_ml_photo(dir):
     for f in glob.glob(dir + '*.jpg'):
         old = f
         new = 'DCIMP/' + basename(f)
@@ -40,15 +40,24 @@ def move_ml_photo(dir):
             print('rmtree dir ' + path)
             rmtree(path)
 
+def mv_xiaomi_photo(ext):
+    i = 1
+    for f in glob.glob('*.' + ext):
+        date = f.split('_')[1]
+        if not exists(date):
+            mkdir(date)
+
+        old = f
+        new = '%s/%s' % (date, f)
+        rename(old, new)
+        print('i = %d, old = %s, new = %s' % (i, old, new))
+        i += 1
+
 if __name__=='__main__':
     srcPath = sys.argv[1]
     chdir(srcPath)
 
-    # for ext in ['jpg', 'mp4']:
-        # rename_files(ext)
-    # replace_filename()
-
-    # for i in range(0, 9):
-        # move_ml_photo('DCIM0%d/' % (i))
+    for ext in ('jpg', 'mp4'):
+        mv_xiaomi_photo(ext)
 
     print('end...')
