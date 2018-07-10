8f9569c6eaa79ddd7476a6a8fbea6f75b9f1c697
mahone3297 <329730566@qq.com>
2018-07-10 18:04:13 +0800

    update subtitle.py

--- src/python/subtitle.py
+++ src/python/subtitle.py
@@ -1,8 +1,26 @@
 from os.path import splitext
+import re
 
 def adjst_subtitle(filename, seconds):
     (root, ext) = splitext(filename)
-    
+    if ext == '.ass':
+        process_ass(filename, seconds)
+    elif ext == '.srt':
+        process_srt(filename, seconds)
+    else:
+        print('I cannot process the ext.')
+
+def process_ass(filename, seconds):
+    f = open(filename, 'r')
+    for l in f.readlines():
+        if not re.match('^Dialogue', l):
+            print(l, end='')
+            continue
+
+        r = l.split(',')
+        /////////////
+        print('r1 = %s, r2 = %s' % (r[1], r[2]))
+    f.close()
 
 if __name__ == '__main__':
     filename = '[tvN] Signal.E01.160122.HDTV.H264.720p-WITH.chs.ass'
