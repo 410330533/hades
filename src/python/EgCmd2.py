693841e5f172933d4024cf42aad744d543872d35
mahone3297 <329730566@qq.com>
2018-08-28 16:18:59 +0800

    update src/python/EgCmd2.py

--- src/python/EgCmd2.py
+++ src/python/EgCmd2.py
@@ -1,6 +1,14 @@
 from cmd2 import Cmd
+import cmd2
 
-class App(Cmd):
+class App(cmd2.Cmd):
+    def preloop(self):
+        self.poutput("pre loop here...")
+
+    def precmd(self, line):
+        self.poutput('pre cmd here...')
+        return line
+
     def do_hello(self, line):
         """Says hello. If you provide a name, it will greet you with it."""
         print('hello %s' % (line))
