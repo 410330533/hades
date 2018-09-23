d43a5bc8843e5ebca0c4047f4f279263ae525b81
mahone3297 <329730566@qq.com>
2018-09-24 00:07:18 +0800

    update creator/server.py

--- src/python/creator/server.py
+++ src/python/creator/server.py
@@ -2,22 +2,32 @@ import socket
 import threading
 import socketserver
 
+encoding = 'utf-8'
+
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        data = str(self.request.recv(1024), 'utf-8')
-        cur_thread = threading.current_thread()
-        response = bytes("[Server] {}:{}".format(cur_thread.name, data), 'utf-8')
-        self.request.sendall(response)
+        while True:
+            data = str(self.request.recv(1024), 'utf-8')
+            print('client {}, [{}]'.format(self.client_address, data))
+
+            if data == 'exit':
+                break;
+
+            # cur_thread = threading.current_thread()
+            # response = bytes("[Server] {}:{}".format(cur_thread.name, data), 'utf-8')
+            self.request.sendall(bytes("[Server] {}".format(data), encoding))
 
 if __name__ == '__main__':
     host, port = '0.0.0.0', 10080
-    server = socketserver.ThreadingTCPServer((host, port), ThreadedTCPRequestHandler)
-    # ip, port = server.server_address
-    print("host = {}, port = {}".format(host, port))
-    t = threading.Thread(target = server.serve_forever)
-    t.daemon = True
-    t.start()
-    print("Server loop running in thread:", t.name)
+    # print("host = {}, port = {}".format(host, port))
+
+    # server = socketserver.ThreadingTCPServer((host, port), ThreadedTCPRequestHandler)
+    server = socketserver.TCPServer((host, port), ThreadedTCPRequestHandler)
+    
+    # t = threading.Thread(target = server.serve_forever)
+    # t.daemon = True
+    # t.start()
+    # print("Server loop running in thread:", t.name)
     # t.join()
 
     server.serve_forever()
